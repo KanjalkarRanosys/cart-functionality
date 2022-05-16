04a03ca4d50bb0847ed1b8c589fb0abc3ca7dc35
KanjalkarRanosys <avinash.kanjalkar@ranosys.com>
2022-05-16 19:41:48 +0530

    formatting and add loading text on popup

--- sample-pwa/src/components/customHeader/searchPopup.js
+++ sample-pwa/src/components/customHeader/searchPopup.js
@@ -1,57 +1,76 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { fullPageLoadingIndicator } from '../../venia-ui/lib/components/LoadingIndicator';
 
-const SearchPopup = ({open, searchingResults, handleClose, handleSearchProducts, filteredSearchProducts, searchingProducts}) => {
+const SearchPopup = ({
+    open,
+    searchingResults,
+    handleClose,
+    handleSearchProducts,
+    filteredSearchProducts,
+    searchingProducts
+}) => {
     if (!open) {
-        return null
+        return null;
     }
 
-    console.log(searchingResults);
-    
-  return (
-    <div>
-        {/* {searchingProducts && searchingProducts.loading ? <div>{fullPageLoadingIndicator}</div> : */}
-        <form onSubmit={filteredSearchProducts}>
-            <div className='searching-items'>
-                <div className='search-box-close-button'
-                    onClick={()=> handleClose() }
-                >  <span>✖</span></div>
-                <div className='search-products'>
-                    <div className='input-search'>
-                    <input
-                    autoFocus
-                    placeholder='search' 
-                        type="text" 
-                        className='search-input' 
-                        onChange={(e)=> {
-                            handleSearchProducts(e)
-                        }}
-                    />
+    console.log(searchingProducts);
+
+    return (
+        <div>
+            {/* {searchingProducts && searchingProducts.loading ? <div>{fullPageLoadingIndicator}</div> : */}
+            <form onSubmit={filteredSearchProducts}>
+                <div className="searching-items">
+                    <div
+                        className="search-box-close-button"
+                        onClick={() => handleClose()}
+                    >
+                        {' '}
+                        <span>✖</span>
                     </div>
-                    <div className='searching-list'>
-                    {searchingResults && searchingResults.items && searchingResults.items.map((el)=> (
-                        <div className='searching-one-product'>
-                        
-                            <div>
-                                <Link 
-                                    onClick={()=> handleClose()}
-                                    to={`/view-product/${el.url_key}`}>
-                                    <img src={el.small_image.url} />
-                                </Link>
-                            </div>
-                        
-                        <div>{el.name}</div>
-                        <div>${el.price.regularPrice.amount.value}</div>
+                    <div className="search-products">
+                        <div className="input-search">
+                            <input
+                                autoFocus
+                                placeholder="search"
+                                type="text"
+                                className="search-input"
+                                onChange={e => {
+                                    handleSearchProducts(e);
+                                }}
+                            />
                         </div>
-                    ))  } 
+                        {searchingProducts && !searchingProducts.loading ?
+                        <div className="searching-list">
+                            {searchingResults &&
+                                searchingResults.items &&
+                                searchingResults.items.map(el => (
+                                    <div className="searching-one-product">
+                                        <div>
+                                            <Link
+                                                onClick={() => handleClose()}
+                                                to={`/view-product/${
+                                                    el.url_key
+                                                }`}
+                                            >
+                                                <img src={el.small_image.url} />
+                                            </Link>
+                                        </div>
+
+                                        <div>{el.name}</div>
+                                        <div>
+                                            $
+                                            {el.price.regularPrice.amount.value}
+                                        </div>
+                                    </div>
+                                ))}
+                        </div> : <div>Loading...</div>}
                     </div>
                 </div>
-            </div>
-        </form>
-    {/* } */}
-    </div>
-  )
-}
+            </form>
+            {/* } */}
+        </div>
+    );
+};
 
-export default SearchPopup
+export default SearchPopup;
